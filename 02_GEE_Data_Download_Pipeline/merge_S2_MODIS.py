5283888d03460dd99216fe2b7c1d295c2302f2ae
varunkt91 <varunkt91@gmail.com>
2025-12-23 00:01:12 -0500

    updated s2_modis
    updated s2_modis

--- 02_GEE_Data_Download_Pipeline/merge_S2_MODIS.py
+++ 02_GEE_Data_Download_Pipeline/merge_S2_MODIS.py
@@ -1,46 +1,5 @@
 import ee
 
-# Merge matched pairs
-# def merge_images(pair):
-#     s2_img = ee.Image(pair.get('primary'))
-#     modis_img = ee.Image(pair.get('secondary'))
-    
-#     modis_proj = modis_img.projection()
-    
-#     s2_resampled = s2_img.resample('bilinear').reproject(crs=modis_proj.crs(), scale=10)
-    
-#     # Create combined mask
-#     overlap_mask = s2_resampled.select('B4').mask().And(modis_img.mask())
-    
-#     s2_masked = s2_resampled.updateMask(overlap_mask)
-#     modis_masked = modis_img.updateMask(overlap_mask).rename('MODIS_Albedo_WSA_shortwave')
-    
-#     return (s2_masked.addBands(modis_masked)
-#                      .copyProperties(s2_img, ['system:time_start', 'date']))
-
-
-
-
-
-
-# def merge_images(pair):
-#     s2_img = ee.Image(pair.get('primary'))
-#     modis_img = ee.Image(pair.get('secondary'))
-
-#     modis_proj = modis_img.projection()
-
-#     # Reproject S2 to match MODIS projection (includes CRS and nominal scale)
-#     s2_resampled = s2_img.resample('bilinear').reproject(modis_proj)
-
-#     # Combined valid mask from both images
-#     overlap_mask = s2_resampled.select('B4').mask().And(modis_img.mask())
-
-#     s2_masked = s2_resampled.updateMask(overlap_mask)
-#     modis_masked = modis_img.updateMask(overlap_mask).rename('MODIS_Albedo_WSA_shortwave')
-
-#     return (s2_masked.addBands(modis_masked)
-#                      .copyProperties(s2_img, ['system:time_start', 'date']))
-
 
 def merge_images(pair):
     # Get Sentinel-2 and MODIS images from the pair
